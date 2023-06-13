9ad39b10bce3a161c31704ce7608403a84d834eb
tombealby <tom.bealby67@gmail.com>
2023-06-13 18:23:51 +0100

    Set up branch with maven.

---
+++ src/main/java/cucumber_expt/cucumber_expt/App.java
@@ -0,0 +1,13 @@
+package cucumber_expt.cucumber_expt;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/stepdefs/CheckoutSteps.java
@@ -0,0 +1,27 @@
+package stepdefs;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class CheckoutSteps {
+	
+	@Given("the price of a {string} is 40c")
+	public void the_price_of_a_is_40c(String string) {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("I checkout {int} {string}")
+	public void i_checkout(Integer int1, String string) {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+	@Then("the total price should be 40c")
+	public void the_total_price_should_be_40c() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+
+}
